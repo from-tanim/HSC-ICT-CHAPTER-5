c3a3acc2e613ae705a46f8164b2507d5a4b83fd4
Tanim <88611927+from-tanim@users.noreply.github.com>
2024-10-16 00:41:10 +0600

    Update multiple_maths.c

--- multiple_maths.c
+++ multiple_maths.c
@@ -7,8 +7,9 @@ int main()
 a=12523,b=123;
 printf("1.Here is the sum:%d\n",a+b);
 printf("2.Here is the sub:%d\n",a-b);
-printf("3.Here is the division:%f\n",a/b);
+printf("3.Here is the division:%.2f\n",(float)a/b);
 printf("4.Here is the multipication:%d\n",a*b);
 
 return 0;
 }
+
