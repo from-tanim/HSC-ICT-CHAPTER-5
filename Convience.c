5d1dd6e7f8bb5c8a2134605a8e0d8a4477c5d120
Tanim <88611927+from-tanim@users.noreply.github.com>
2024-10-23 05:15:28 +0600

    Create Convience.c

---
+++ Convience.c
@@ -0,0 +1,13 @@
+//Convience //
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int n;
+    printf("Enter the value of n:");
+    scanf("%d", &n);
+    for(int i=1;i<=1000000;i++){
+        printf("%d Sorry\n",i);
+    }
+    return 0;
+    
+}
