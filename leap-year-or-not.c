bf39cb93ab1e7b0c100d464d62e096c90e4f1ed9
Tanim <88611927+from-tanim@users.noreply.github.com>
2024-10-23 03:55:41 +0600

    Summarizing

--- leap-year-or-not.c
+++ leap-year-or-not.c
@@ -3,14 +3,18 @@
 #include<math.h>
 int main()
 {
-  int a;
-printf("Enter the year:");
-scanf("%d",&a);
-if((a % 4 == 0 && a % 100 !=0) || (a % 400 == 0)){
-printf("%d is a leap year",a);
-}
-else{
-printf("%d is not a leap year",a);
-}
-return 0;
+    int y;
+    printf("Enter the year:");
+    scanf("%d",&y);
+    
+    if(y%400==0){
+        printf("%d is a leap year",y);
+    }
+    else if(y%100!=0 && y%4==0){
+        printf("%d is a leap year",y);
+    }
+    else {
+        printf("%d is not a leap year",y);
+    }
+    return 0;
 }
